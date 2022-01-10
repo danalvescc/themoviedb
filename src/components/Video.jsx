66a6998ef0e2727d1fa22360b3706856437cf7e8
Daniel Alves <danalvescc@gmail.com>
2022-01-10 12:55:32 -0300

    Feat(movieVideo): remove react-youtube

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,13 +1,18 @@
-import Youtube from 'react-youtube';
 import styles from '../styles/components/Video.module.css'
 
 function Video({videoID}){
     return (
         <div className={styles.container}>
-            {videoID != ''&&
-                <Youtube videoId={videoID} opts={{
-                    width: '100%'
-                }}/>
+            {videoID &&
+                <iframe
+                width="100%"
+                height="480"
+                src={`https://www.youtube.com/embed/${videoID}`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
             }
         </div>
     )
